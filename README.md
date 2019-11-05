9d8d67047068ad92be4487128af08f267dcdf401
congweitao <congweitao@hotmail.com>
2019-11-05 07:46:13 -0500

    add ref

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # hula
+
+# Reference
+[Intel QAT&OpenSSL Performance](https://01.org/sites/default/files/downloads/intelr-quickassist-technology/intelquickassisttechnologyopensslperformance.pdf)
--- parse.py
+++ parse.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
-
 import yaml
 
-filename="./xyz.yaml"
+file_name = "./xyz.yaml"
 
-with open(filename,'r') as stream:
-	try:
-		data=yaml.safe_load(stream)
-		print(data['Total Program Time'])
-		print(data["pets"][1])
-	except yaml.YAMLError as exec:
-		print(exec)
+with open(file_name, 'r') as stream:
+    try:
+        s_data = yaml.safe_load(stream)
+        print(s_data['Total Program Time'])
+        print(s_data["pets"][1])
+    except yaml.YAMLError as exec:
+        print(exec)
