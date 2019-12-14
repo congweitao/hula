74e1d239b4787199380eb3599d4571856e10e8a6
congweitao <congweitao@hotmail.com>
2019-12-14 04:47:24 -0500

    add test

--- gluster/smallfile.sh
+++ gluster/smallfile.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/bash
 
-python smallfile_cli.py --operation create --threads 8 --file-size 1024 --files 2048 --top /mnt
+python smallfile_cli.py --operation create --threads 8 --file-size 1024 --files 2048 --top /mnt --host-set osd1,osd2,osd3
+
+gluster volume set gv0 features.cache-invalidation on
+gluster volume set gv0 features.cache-invalidation-timeout 600
+gluster volume set gv0 performance.stat-prefetch on
+gluster volume set gv0 performance.cache-invalidation on
