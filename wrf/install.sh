fe2c21968c79d096c8da1ced8b39112951797d1c
congweitao <congweitao@hotmail.com>
2019-12-14 06:34:36 -0500

    add wrf

--- wrf/install.sh
+++ wrf/install.sh
@@ -14,3 +14,15 @@ yum -y install netcdf-fortran-gnu8-openmpi3-ohpc.aarch64
 
 ln -s /opt/ohpc/pub/libs/gnu8/openmpi3/netcdf-fortran/4.5.2/include/netcdf.inc /opt/ohpc/pub/libs/gnu8/openmpi3/netcdf/4.7.1/include/
 
+# setup 
+export NETCDF=/opt/ohpc/pub/libs/gnu8/openmpi3/netcdf/4.7.1/
+export NETCDF_FORTRAN=/opt/ohpc/pub/libs/gnu8/openmpi3/netcdf-fortran/4.5.2/
+export PNETCDF=/opt/ohpc/pub/libs/gnu8/openmpi3/pnetcdf/1.12.0/
+export HDF5=/opt/ohpc/pub/libs/gnu8/openmpi3/hdf5/1.10.5/
+export LD_LIBRARY_PATH=${NETCDF}/lib:${NETCDF_FORTRAN}/lib:${PNETCDF}/lib:${HDF5}/lib:${LD_LIBRARY_PATH}
+
+export WRFIO_NCD_LARGE_FILE_SUPPORT=1
+
+
+
+
